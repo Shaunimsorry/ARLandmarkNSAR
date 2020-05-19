479eb1e6b0c924075da6a2a32218d68a8c2a775f
Tankbusta_91 <shaunansari@gmail.com>
2020-05-19 07:49:19 +0500

    Pull push to Mapbox Dataset working

---
+++ Assets/Scripts/MapBoxAPIFeatureClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class mapboxFeatureClass
+{
+    public string type;
+    public Properties properties;
+    public Geometry geometry;
+    public string id;
+}
+
+[System.Serializable]
+public class Geometry
+{
+    [SerializeField]
+    public List<double> coordinates;
+    public string type;
+}
+
+[System.Serializable]
+public class Properties
+{
+    public string name;
+}
---
+++ Assets/Scripts/MapBoxLandMarkClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// [System.Serializable]
+// public class MapBoxGEOJSON_GET
+// {
+//     public string type;
+//     public List<Feature> features;
+// }   
+
+// [System.Serializable]
+// public class Feature
+// {
+//     public string id;
+//     public string type;
+//     public Properties properties;
+//     public Geometry geometry;
+// }
+// [System.Serializable]
+// public class Geometry
+// {
+//     public List<double> coordinates;
+//     public string type;
+// }
+// [System.Serializable]
+// public class Properties
+// {
+//     public string name;
+// }
