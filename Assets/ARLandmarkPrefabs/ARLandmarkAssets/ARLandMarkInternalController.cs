71395ee38d6b6618f0f09ba9b7c5405287d4571d
Tankbusta_91 <shaunansari@gmail.com>
2020-05-21 21:21:04 +0500

    Landmark Shapes Added

--- Assets/ARLandmarkPrefabs/ARLandmarkAssets/ARLandMarkInternalController.cs
+++ Assets/ARLandmarkPrefabs/ARLandmarkAssets/ARLandMarkInternalController.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class ARLandMarkInternalController : MonoBehaviour
 {
     public Camera targetCamera;
+
+    //Setup Landmark UpperLogo Assets
+    public GameObject landmark_triangleLogo;
+    public GameObject landmark_Squarelogo;
+    public GameObject landmark_circleLogo;
+
+    public string landmarkLogo;
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     { 
         if(!targetCamera)
         {
             targetCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
-        } 
+        }
+
+        landmark_circleLogo.SetActive(false);
+        landmark_Squarelogo.SetActive(false);
+        landmark_triangleLogo.SetActive(false);
+         
     }
 
     // Update is called once per frame
@@ -23,5 +42,29 @@ public class ARLandMarkInternalController : MonoBehaviour
         relativepos.y = 0;
         Quaternion deducedRotation = Quaternion.LookRotation(relativepos);;
         transform.rotation = deducedRotation;
+
+        //Setup Landmark Type Switcher
+        if(landmarkLogo == "Square")
+        {
+                landmark_circleLogo.SetActive(false);
+                landmark_Squarelogo.SetActive(true);
+                landmark_triangleLogo.SetActive(false);
+        }else
+        {
+            if(landmarkLogo == "Circle")
+            {
+                landmark_circleLogo.SetActive(true);
+                landmark_Squarelogo.SetActive(false);
+                landmark_triangleLogo.SetActive(false);
+            }else
+            {
+                if(landmarkLogo == "Triangle")
+                {
+                    landmark_circleLogo.SetActive(false);
+                    landmark_Squarelogo.SetActive(false);
+                    landmark_triangleLogo.SetActive(true);    
+                }
+            }
+        }
     }
 }
